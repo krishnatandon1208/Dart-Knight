9ac1d50162cf494308e38590667994445de97e20
Krishna Tandon <krishnatandon.1208@gmail.com>
2020-12-13 05:10:10 +0530

    Convert String to Number

--- Questions/Question-1.dart
+++ Questions/Question-1.dart
@@ -1,9 +1,11 @@
 //WAP to accept a number in string format and return a number.
+//"123" => 124
 
 int stringToNumber(String str) {
-	return (int.parse(str));
+  return (int.parse(str));
 }
 
+String number = "123456";
 void main() {
-	print(stringToNumber("1234"));
+  print(stringToNumber(number));
 }
