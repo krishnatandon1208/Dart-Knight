455c004738d472b4cad60e2c965368786810d7d5
Krishna Tandon <krishnatandon.1208@gmail.com>
2020-12-13 06:39:19 +0530

    Finding bug in text

---
+++ Questions/Question-8.dart
@@ -0,0 +1,16 @@
+/**
+ * Basic variable assignment
+ * This code should store "codewa.rs" as a variable called name but it's not working. 
+ * Can you figure out why?
+ */
+
+void main() {
+  String a = "code";
+  print(a);
+
+  String b = "wa.rs";
+  print(b);
+
+  String name = a + b;
+  print(name);
+}
