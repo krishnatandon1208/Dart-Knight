e72784013007b13d284742678c8793b219c65cae
Krishna Tandon <krishnatandon.1208@gmail.com>
2020-12-15 05:58:27 +0530

    Number to String

---
+++ Questions/Question-14.dart
@@ -0,0 +1,12 @@
+/**
+ * Convert a Number to a String!
+ * We need a function that can transform a number into a string.
+ */
+
+void main() {
+  print(numToString(123));
+}
+
+String numToString(num) {
+  return ('"${num}"');
+}
